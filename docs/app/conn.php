6dd95a6ae76ef00529cad800a566851a6f229593
Carlos Aleman <caleman9791@gmail.com>
2023-07-16 02:04:37 -0400

    Plantilla

---
+++ docs/app/conn.php
@@ -0,0 +1,13 @@
+<?php
+
+if ($_SERVER['SERVER_NAME'] == "localhost") {
+	$servername = "localhost";
+	$username = "username";
+	$password = "password";
+	$dbname = "caleman9791_tutoriales";
+} else {
+	$servername = "sql108.epizy.com";
+	$username = "epiz_34280086";
+	$password = "64ywnoop8R8FUr";
+	$dbname = "epiz_34280086_tutoriales";
+}
