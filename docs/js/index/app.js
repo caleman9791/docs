122b86f889d982c25dfa62ebc9ccddb3ef800a7c
Carlos Aleman <caleman9791@gmail.com>
2023-07-16 04:38:56 -0400

    Interfaz.

--- docs/js/index/app.js
+++ docs/js/index/app.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", (event) => {
 	console.log("DOM fully loaded and parsed");
 	let user_nav = document.getElementById("user_nav");
-	user_nav.style.display = "none";
+	// user_nav.style.display = "none";
 });
